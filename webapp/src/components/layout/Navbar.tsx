dfa4cfb907d93a2509339eb852deb0c4091e9aa2
Kelton Furtado <widuca.kc@gmail.com>
2022-05-28 11:25:22 -0100

    create menu coponent

---
+++ webapp/src/components/layout/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+export default function Navbar() {
+    return (
+        <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
+
+            <title>TODO PROJECTS</title>
+
+            <div className="container flex justify-between h-16 mx-auto">
+                <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2 text-4xl">
+                    Task Manager
+                </a>
+
+                <div className="items-center flex-shrink-0 hidden lg:flex">
+                    <button className="self-center px-8 py-3 rounded">Sign in</button>
+                    <button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Sign up</button>
+                </div>
+                <button className="p-4 lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
+        </header>
+    )
+}
