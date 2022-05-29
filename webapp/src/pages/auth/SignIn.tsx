f43ec19f75763341d62288d5d82c26f219c72686
Kelton Furtado <widuca.kc@gmail.com>
2022-05-29 00:22:41 -0100

    ui improvement

--- webapp/src/pages/auth/SignIn.tsx
+++ webapp/src/pages/auth/SignIn.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../lib/auth';
 
 const SignIn: React.FC = () => {
@@ -12,8 +13,8 @@ const SignIn: React.FC = () => {
     }
 
     return (
-        <>
-            <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-900 dark:text-gray-100">
+        <div className='flex flex-col justify-center items-center w-screen h-screen bg-gray-200 dark:bg-gray-900'>
+            <div className="flex flex-col lg:w-1/3 md:lg:w-1/3 max-w-md p-6 shadow-lg rounded-md bg-gray-100 sm:p-10 dark:bg-gray-800 dark:text-gray-100">
                 <div className="mb-8 text-center">
                     <h1 className="my-3 text-4xl font-bold">Sign in</h1>
                     <p className="text-sm dark:text-gray-400">Sign in to access your account</p>
@@ -21,7 +22,7 @@ const SignIn: React.FC = () => {
                 <form novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-4">
                         <div>
-                            <label for="email" className="block mb-2 text-sm">Email / Username</label>
+                            <label for="email" className="block mb-2 text-sm">Email</label>
                             <input value={email} type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} placeholder="user@site.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
                         </div>
                         <div>
@@ -34,15 +35,15 @@ const SignIn: React.FC = () => {
                     </div>
                     <div className="space-y-2">
                         <div>
-                            <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900" onClick={handleSignIn} >Sign In</button>
+                            <button type="button" className="w-full px-8 py-3 shadow hover:bg-violet-700 hover:text-white font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900" onClick={handleSignIn} >Sign In</button>
                         </div>
                         <p className="px-6 text-sm text-center dark:text-gray-400">Don't have an account yet?
-                            <a rel="noopener noreferrer" href="" className="hover:underline dark:text-violet-400">Sign up</a>.
+                            <Link to={'/register'} className='hover:underline dark:text-violet-400'> Sign up </Link>
                         </p>
                     </div>
                 </form>
             </div>
-        </>
+        </div>
     );
 }
 
--- webapp/src/pages/auth/SignUp.tsx
+++ webapp/src/pages/auth/SignUp.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useAuth } from '../../lib/auth';
 const SignUp: React.FC = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { signUp, errors, loading } = useAuth()
+
+    const handleSignUp = () => {
+        signUp(name, email, password)
+    }
+
     return (
-        <>
-            <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-900 dark:text-gray-100">
+        <div className='flex flex-col justify-center items-center w-screen h-screen bg-gray-200 dark:bg-gray-900'>
+            <div className="flex flex-col lg:w-1/3 md:w-1/3 max-w-md p-6 shadow-lg rounded-md bg-gray-100 sm:p-10 dark:bg-gray-800 dark:text-gray-100">
                 <div className="mb-8 text-center">
                     <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
-                    <p className="text-sm dark:text-gray-400">Sign in to access your account</p>
+                    <p className="text-sm dark:text-gray-400">Create account now and enjoy it!</p>
                 </div>
                 <form novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-4">
                         <div>
-                            <label for="email" className="block mb-2 text-sm">Email / Username</label>
+                            <label for="name" className="block mb-2 text-sm">Name</label>
+                            <input type="name" name="name" id="name" placeholder="Fabienne Andrade" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                        </div>
+                        <div>
+                            <label for="email" className="block mb-2 text-sm">Email</label>
                             <input type="email" name="email" id="email" placeholder="user@site.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
                         </div>
                         <div>
@@ -24,15 +40,15 @@ const SignUp: React.FC = () => {
                     </div>
                     <div className="space-y-2">
                         <div>
-                            <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">Sign in</button>
+                            <button type="button" className="w-full px-8 py-3 shadow hover:bg-violet-700 hover:text-white font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900" onClick={handleSignUp}>Sign Un</button>
                         </div>
-                        <p className="px-6 text-sm text-center dark:text-gray-400">Don't have an account yet?
-                            <a rel="noopener noreferrer" href="" className="hover:underline dark:text-violet-400">Sign up</a>.
+                        <p className="px-6 text-sm text-center dark:text-gray-400">Do you already have account?
+                            <Link to={'/'} className='hover:underline dark:text-violet-400'> Sign In Here</Link>
                         </p>
                     </div>
                 </form>
             </div>
-        </>
+        </div>
     );
 }
 
