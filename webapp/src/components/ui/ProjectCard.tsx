c2976fe6a030f6bb512878912e4b8b092b481e78
Kelton Furtado <widuca.kc@gmail.com>
2022-05-29 22:48:37 -0100

    delete project and add icons

--- webapp/src/components/ui/ProjectCard.tsx
+++ webapp/src/components/ui/ProjectCard.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { FiEdit, FiPlus, FiTrash } from 'react-icons/fi'
+import { toast } from 'react-toastify';
+import { fetchJson } from '../../lib/api';
 
 import { IProject, ITask } from '../../lib/types';
 import CreateTaskModal from './CreateTaskModal';
@@ -22,30 +25,46 @@ const ProjectCard = ({ project, loadProjects }: IProps) => {
         setTaskForm({ isVisible: true, task })
     }
 
+    async function handleDeleteProject() {
+        const response = await fetchJson({ url: `projects/${project?.id}`, method: 'DELETE' })
+
+        if (response.status === 'SUCCESS') {
+            toast.success('Project deleted successfully!', {
+                position: toast.POSITION.TOP_CENTER
+            });
+            loadProjects()
+        } else {
+            toast.error('Error deleting Project!', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+    }
+
     return (
-        <div className="rounded-md shadow-lg px-2 my-4 sm:w-96 dark:bg-gray-900 dark:text-gray-100">
+        <div className="w-full ms:w-1/4 md:w-96 lg:w-96 rounded-md shadow-lg px-2 my-2 dark:bg-gray-900 dark:text-gray-100">
             <div className="flex items-center justify-between p-3">
                 <div className="flex items-center space-x-2">
                     <div className="-space-y-1">
-                        <h2 className="text-xl font-semibold leading-none">{project?.title}</h2>
-                        <span className="inline-block text-xs leading-none dark:text-gray-400">{project?.description}</span>
+                        <h2 className="text-lg font-semibold leading-none">{project?.title}</h2>
                     </div>
                 </div>
-                <div className="flex items-center space-x-2">
+                <div className="flex items-center space-x-1">
                     <button type="button" className="flex items-center px-2 py-1 space-x-1 text-xs" onClick={() => setTaskFormVisible(null)}>
-                        <svg xmlns="http://www.w3.org/2000setTaskFormVisible/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                            <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                        </svg>
-                        <span>Edit</span>
+                        <FiEdit className="w-4 h-4" />
+                    </button>
+
+                    <button type="button" className="flex rounded-sm  items-center px-1 py-1 space-x-1 text-xs" onClick={handleDeleteProject}>
+                        <FiTrash className="w-4 h-4" />
                     </button>
 
                     <button type="button" className="flex items-center px-2 py-1 space-x-1 text-xs" onClick={() => setTaskFormVisible(null)}>
-                        <svg xmlns="http://www.w3.org/2000setTaskFormVisible/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                            <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                        </svg>
+                        <FiPlus className="w-4 h-4" />
                         <span>New Task</span>
                     </button>
                 </div>
+            </div>
+            <div className="flex items-center justify-between px-3">
+                <span className="w-full break-words inline-block text-xs leading-none dark:text-gray-400">{project?.description}</span>
             </div>
 
             <div className="w-full h-80 overflow-y-auto">
@@ -55,6 +74,7 @@ const ProjectCard = ({ project, loadProjects }: IProps) => {
                             key={task.id}
                             task={task}
                             setTaskFormVisible={setTaskFormVisible}
+                            loadProjects={loadProjects}
                         />)
                     }
                 </ul>
--- webapp/src/components/ui/TaskCard.tsx
+++ webapp/src/components/ui/TaskCard.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
+import { FiEdit, FiPlus, FiTrash, FiCheck } from 'react-icons/fi'
+import { toast } from 'react-toastify';
+import { fetchJson } from '../../lib/api';
+
+
 import { ITask } from '../../lib/types';
+import Badge from './Badge';
 
 // import { Container } from './styles';
 interface IProps {
     task: ITask
-    setTaskFormVisible:(task:ITask|null) => void
+    setTaskFormVisible: (task: ITask | null) => void
+    loadProjects: () => void
 }
-const TaskCard = ({ task , setTaskFormVisible}: IProps) => {
+const TaskCard = ({ task, setTaskFormVisible, loadProjects }: IProps) => {
+
+    async function handleMarkAsDone() {
+        const response = await fetchJson({ url: `tasks/${task?.id}`, method: 'PUT', data: { status: 'DONE' } })
+
+        if (response.status === 'SUCCESS') {
+            toast.success('Task marked as done!', {
+                position: toast.POSITION.TOP_CENTER
+            });
+            loadProjects()
+        } else {
+            toast.error('Error marking Task done!', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+    }
+
+    async function handleDeleteTask() {
+        const response = await fetchJson({ url: `tasks/${task?.id}`, method: 'DELETE' })
+
+        if (response.status === 'SUCCESS') {
+            toast.success('Task deleted successfully!', {
+                position: toast.POSITION.TOP_CENTER
+            });
+            loadProjects()
+        } else {
+            toast.error('Error deleting Task!', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+    }
+
     return (
-        <li className="flex flex-col py-3 sm:flex-row sm:justify-between">
+        <li className="flex flex-col py-2 sm:flex-row sm:justify-between">
             <div className="flex w-full space-x-2 sm:space-x-4">
-                
+
                 <div className="flex flex-col justify-between w-full pb-4">
                     <div className="flex justify-between w-full pb-2 space-x-2">
                         <div className="space-y-1">
@@ -18,35 +56,39 @@ const TaskCard = ({ task , setTaskFormVisible}: IProps) => {
                             <p className="text-sm dark:text-gray-400">{task?.description}</p>
                         </div>
                         <div className="text-right">
+                            <Badge status={task.status} />
                             <p className="text-sm line-through dark:text-gray-600">{task && task?.dateEnd?.toLocaleString()}</p>
                         </div>
                     </div>
+                    {
+                        task.status !== 'DONE' && (
 
-                    <div className="flex justify-between text-sm ">
-                        <button type="button" className="flex items-center px-2 py-1 space-x-1">
-                            <svg xmlns="http://www.w3.org/2000setTaskFormVisible/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                                <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                            </svg>
-                            <span>Done</span>
-                        </button>
-                        <button type="button" className="flex items-center px-2 py-1 space-x-1" onClick={() => setTaskFormVisible(task)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                                <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
-                            </svg>
-                            <span>Edit</span>
-                        </button>
+                            <div className="flex justify-between text-sm ">
+                                <button type="button" className="flex items-center px-2 py-1 space-x-1" onClick={handleMarkAsDone}>
+                                    <FiCheck className="w-4 h-4" />
+                                    <span>Done</span>
+                                </button>
+                                <button type="button" className="flex items-center px-2 py-1 space-x-1" onClick={() => setTaskFormVisible(task)}>
+                                    <FiEdit className="w-4 h-4" />
+                                    <span>Edit</span>
+                                </button>
 
-                        <button type="button" className="flex items-center px-2 py-1 pl-1 space-x-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-                                <path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
-                                <rect width="32" height="200" x="168" y="216"></rect>
-                                <rect width="32" height="200" x="240" y="216"></rect>
-                                <rect width="32" height="200" x="312" y="216"></rect>
-                                <path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
-                            </svg>
-                            <span>Delete</span>
-                        </button>
-                    </div>
+                                <button type="button" className="flex items-center px-2 py-1 pl-1 space-x-1" onClick={handleDeleteTask}>
+                                    <FiTrash className="w-4 h-4" />
+                                    <span>Delete</span>
+                                </button>
+                            </div>
+                        )
+                    }
+
+                    {
+                        task.status === 'DONE' && (
+
+                            <div className="flex justify-end text-sm ">
+                                <span className="text-sm font-semibold leading-snug sm:pr-2">Finished At: </span> <span className="text-sm">{new Date(task.updatedAt).toLocaleString()}</span>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </li>
