862f1adb26d29927f6cdcc93c9e625a422578e83
Kelton Furtado <widuca.kc@gmail.com>
2022-05-29 18:36:07 -0100

    add tostify  container globally

--- webapp/src/components/layout/MainLayout.tsx
+++ webapp/src/components/layout/MainLayout.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 // import { Container } from './styles';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { ToastContainer } from 'react-toastify';
 
 export default function MainLayout({ children }) {
     return <>
         <Navbar />
         <main className="dark:bg-gray-800 dark:text-cyan-50 w-full">
+            <ToastContainer
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={true}
+                closeOnClick
+            />
             <div>{children}</div>
         </main>
         <Footer />
