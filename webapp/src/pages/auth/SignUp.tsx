f508e561e1165daf3769983ca285b69a87902585
Kelton Furtado <widuca.kc@gmail.com>
2022-05-29 01:57:02 -0100

    signup done

--- webapp/src/pages/auth/SignUp.tsx
+++ webapp/src/pages/auth/SignUp.tsx
@@ -17,7 +17,6 @@ const SignUp: React.FC = () => {
     }
 
     useEffect(() => {
-        console.log('errors>', errors)
         if (errors !== null) {
             toast.error(errors[0], {
                 position: toast.POSITION.TOP_CENTER
@@ -38,22 +37,22 @@ const SignUp: React.FC = () => {
                     <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
                     <p className="text-sm dark:text-gray-400">Create account now and enjoy it</p>
                 </div>
-                <form novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
+                <form  action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-4">
                         <div>
-                            <label for="name" className="block mb-2 text-sm">Name</label>
-                            <input type="name" name="name" id="name" placeholder="Fabienne Andrade" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                            <label htmlFor="name" className="block mb-2 text-sm">Name</label>
+                            <input type="name" name="name" id="name" placeholder="Fabienne Andrade" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" value={name} onChange={(e) => setName(e.target.value)} />
                         </div>
                         <div>
-                            <label for="email" className="block mb-2 text-sm">Email</label>
-                            <input type="email" name="email" id="email" placeholder="user@site.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                            <label htmlFor="email" className="block mb-2 text-sm">Email</label>
+                            <input type="email" name="email" id="email" placeholder="user@site.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" value={email} onChange={(e) => setEmail(e.target.value)} />
                         </div>
                         <div>
                             <div className="flex justify-between mb-2">
-                                <label for="password" className="text-sm">Password</label>
+                                <label htmlFor="password" className="text-sm">Password</label>
                                 <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">Forgot password?</a>
                             </div>
-                            <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+                            <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" value={password} onChange={(e) => setPassword(e.target.value)}  />
                         </div>
                     </div>
                     <div className="space-y-2">
