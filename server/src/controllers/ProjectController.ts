8ca318bf3c1430c9df807e27818a6450814be02c
Kelton Furtado <widuca.kc@gmail.com>
2022-05-30 00:44:33 -0100

    order task on get all relatinoshion by status

--- server/src/controllers/ProjectController.ts
+++ server/src/controllers/ProjectController.ts
@@ -36,14 +36,41 @@ export class ProjectController {
     async all(req: Request, res: Response) {
         const userId = req.userId
 
-        const projects = await prisma.project.findMany({ where: { userId }, include: { tasks: true } })
+        const projects = await prisma.project.findMany({
+            where: {
+                userId
+            },
+            include: {
+                tasks: {
+                    orderBy: {
+                        status: 'asc',
+                    }
+                }
+            }
+        })
 
         return res.status(200).json(projects)
     }
 
     async show(req: Request, res: Response) {
         const { id } = req.params
-        const project = await prisma.project.findFirst({ where: { id: Number(id) }, include: { tasks: true } })
+        const project = await prisma.project.findFirst({
+            where: {
+                id: Number(id)
+            },
+            include: {
+                tasks: {
+                    orderBy: {
+                        status: 'desc',
+                        createdAt: 'asc'
+                    },
+                    select: {
+                        title: true,
+                        description:true
+                    },
+                }
+            }
+        })
         return res.status(200).json(project)
     }
 
